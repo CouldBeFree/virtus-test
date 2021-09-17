7a8848c0aa3fa8082ed4c069eef8de5dfa858e20
Viktor <viktoralwaysfirst@gmail.com>
2021-09-17 17:30:30 +0300

    Updated styles of progress bar

--- src/components/Projects/components/projectCard.vue
+++ src/components/Projects/components/projectCard.vue
@@ -7,7 +7,7 @@
 			<span>{{data.title.company}}</span>
 		</div>
 		<div class="project-value">
-			{{ data.title.value }}
+			${{ data.value }}
 		</div>
 		<div class="project-deadline">
 			{{ data.deadline.date }} <br>
@@ -18,9 +18,9 @@
 		</div>
 		<div class="project-progress">
 			{{ data.progress }}%
-      <div class="progress-wrap">
-        <div class="progress-inner" :style="{ width: `${data.progress}%`, backgroundColor: progressColor }"></div>
-      </div>
+			<div class="progress-wrap">
+				<div class="progress-inner" :style="{ width: `${data.progress}%`, backgroundColor: progressColor }"></div>
+			</div>
 		</div>
 		<div class="project-status">
 			{{ data.status.name }}
@@ -47,100 +47,105 @@ export default {
 			required: true
 		}
 	},
-  computed: {
+	computed: {
 		progressColor () {
-			if (this.data.progress < 50) {
-				return 'blue'
-      } else {
-				return 'green'
-      }
-    }
-  }
+			if (this.data.progress > 90) {
+				return '#2196f3'
+			} else {
+				return '#4cae50'
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-  .progress-wrap {
-    width: 100%;
-    height: 10px;
-    background-color: #9EA3B4;
-    position: relative;
-    border-radius: 5px;
-    overflow: hidden;
-  }
+.project-item {
+	color: white;
+	border-radius: 5px;
+	background-color: #3a3e52;
+	border-left: 4px solid #2196f3;
+	padding: 25px 35px 25px 48px;
+	font-size: 16px;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 24px;
+	align-items: center;
+}
+.project-item.quened {
+	border-color: #e1e2e7;
+}
+.project-item.completed {
+	border-color: #4cae50;
+}
+.project-item span {
+	color: #9ea3b4;
+	font-size: 14px;
+}
+.project-title {
+	width: 13.56%;
+}
+.project-value {
+	width: 9.78%;
+}
+.project-deadline {
+	width: 12.78%;
+}
+.project-spent {
+	width: 11.76%;
+}
+.project-progress {
+	width: 23.57%;
+	display: flex;
+	align-items: center;
+	box-sizing: border-box;
+	padding-right: 70px;
+}
+.progress-wrap {
+	width: 100%;
+	height: 10px;
+	background-color: #9EA3B4;
+	position: relative;
+	border-radius: 5px;
+	overflow: hidden;
+	box-sizing: border-box;
+	margin-left: 10px;
+}
+.progress-inner {
+	position: absolute;
+	width: 100%;
+	left: 0;
+	top: 0;
+	height: 100%;
+	border-radius: 4px;
+}
+.project-status {
+	width: 12%;
+}
+.project-assigned {
+	display: flex;
+	width: 16.55%;
+}
+.assignee-image {
+	position: relative;
+	width: 42px;
+	height: 0;
+	padding-bottom: 100%;
+	overflow: hidden;
+}
 
-  .progress-inner {
-    position: absolute;
-    width: 100%;
-    left: 0;
-    top: 0;
-    height: 100%;
-  }
+.assignee-image img {
+	position: absolute;
+	top: 0;
+	left: 0;
+	display: block;
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+}
 
-	.project-item {
-		color: white;
-		border-radius: 5px;
-		background-color: #3a3e52;
-		border-left: 4px solid #2196f3;
-		padding: 25px 35px 25px 48px;
-		font-size: 16px;
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 24px;
-		align-items: center;
-	}
-	.project-item.quened {
-		border-left: 4px solid #e1e2e7;
-	}
-	.project-item.completed {
-		border-left: 4px solid #4cae50;
-	}
-	.project-item span {
-		color: #9ea3b4;
-		font-size: 14px;
-	}
-	.project-title {
-		width: 13.56%;
-	}
-	.project-value {
-		width: 9.78%;
-	}
-	.project-deadline {
-		width: 12.78%;
-	}
-	.project-spent {
-		width: 11.76%;
-	}
-	.project-progress {
-		width: 23.57%
-	}
-	.project-status {
-		width: 12%;
-	}
-	.project-assigned {
-		display: flex;
-		width: 16.55%;
-	}
-	.assignee-image {
-		position: relative;
-		width: 42px;
-		height: 0;
-		padding-bottom: 100%;
-		overflow: hidden;
-	}
-
-	.assignee-image img {
-		position: absolute;
-		top: 0;
-		left: 0;
-		display: block;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-
-	.assignee-info {
-		padding-left: 25px;
-	}
+.assignee-info {
+	padding-left: 25px;
+}
 
 </style>
