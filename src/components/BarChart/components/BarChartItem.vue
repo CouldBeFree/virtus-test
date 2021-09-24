e2161498907f5ce77c4a96dd6fa2f08c76f55ea9
Viktor <viktoralwaysfirst@gmail.com>
2021-09-24 11:36:41 +0300

    formed and styled elements of BarChart

--- src/components/BarChart/components/BarChartItem.vue
+++ src/components/BarChart/components/BarChartItem.vue
@@ -1,21 +1,74 @@
 <template>
-	<div>
-		{{data}}
+	<div class="graph-item">
+		<div class="graph-item__wrap">
+			<div class="graph-item__inner">
+				<div class="graph-item__value">
+					<div class="graph-item__number">{{data.value}}</div>
+					<div class="graph-item__fill" :style="{ height: `${data.value}%` }"></div>
+				</div>
+			</div>
+		</div>
+		<div class="graph-item__name">
+			{{data.name}}
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
 	name: "BarChartItem",
-  props: {
+	props: {
 		data: {
 			required: true,
-      default: () => {}
-    }
-  }
+			default: () => {}
+		}
+	}
 }
 </script>
 
 <style scoped>
-
+.graph-item {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	min-width: 80px;
+}
+.graph-item__wrap {
+	height: 140px;
+	width: 100%;
+	position: relative;
+	display: flex;
+	justify-content: center;
+}
+.graph-item__inner {
+	height: 100%;
+}
+.graph-item__wrap:after {
+	content: '';
+	width: 100%;
+	position: absolute;
+	bottom: -1px;
+	left: 0;
+	height: 1px;
+	background-color: #e1e1e8;
+}
+.graph-item__value {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	height: 100%;
+	color: #21a6c8;
+}
+.graph-item__fill {
+	height: 70%;
+	background-color: #e1e1e8;
+	width: 35px;
+}
+.graph-item__number {
+	text-align: center;
+}
+.graph-item__name {
+	padding: 7px 5px 0 5px;
+	color: #8989ab;
+}
 </style>
--- src/components/BarChart/index.vue
+++ src/components/BarChart/index.vue
@@ -1,10 +1,15 @@
 <template>
-	<div>
-		<BarChartItem
-      v-for="item in chartData"
-      :key="item.key"
-      :data="item"
-    />
+	<div class="graph-block">
+		<h3>Based on company size</h3>
+		<div class="graph-block__inner">
+			<div class="graph-block__average"></div>
+			<BarChartItem
+					v-for="item in chartData"
+					:key="item.key"
+					:data="item"
+			/>
+		</div>
+
 	</div>
 </template>
 
@@ -14,11 +19,11 @@ import BarChartItem from "./components/BarChartItem";
 
 export default {
 	name: "BarChart",
-  data() {
+	data() {
 		return {
 			chartData: null
-    }
-  },
+		}
+	},
 	components: {
 		BarChartItem
 	},
@@ -28,17 +33,37 @@ export default {
 	methods: {
 		getData() {
 			axios.get('/company-size')
-				.then((res) => {
-					this.chartData = res
-				})
-				.catch(e => {
-					console.error(e)
-				})
-    }
-  }
+					.then((res) => {
+						this.chartData = res
+					})
+					.catch(e => {
+						console.error(e)
+					})
+		}
+	}
 }
 </script>
 
 <style scoped>
-
+.graph-block {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background: #2f3242;
+	padding: 20px;
+}
+.graph-block h3 {
+	color: #21a6c8;
+}
+.graph-block__inner {
+	display: flex;
+	position: relative;
+}
+.graph-block__average {
+	position: absolute;
+	left: 0;
+	top: 30%;
+	width: 100%;
+	border-bottom: 1px dashed #21a6c8;
+}
 </style>
