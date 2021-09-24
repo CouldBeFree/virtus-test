58f7bdd825dfaedad3beb00c57267d52f682c206
Viktor <viktoralwaysfirst@gmail.com>
2021-09-24 08:50:15 +0300

    bar chart

---
+++ src/components/BarChart/components/BarChartItem.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>Chart item</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BarChartItem"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BarChart/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<BarChartItem />
+	</div>
+</template>
+
+<script>
+import BarChartItem from "./components/BarChartItem";
+
+export default {
+	name: "BarChart",
+	components: {
+		BarChartItem
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
